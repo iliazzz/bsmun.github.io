62ce31a8c10fc7c7b2eb60c10adfe063f9c562df
Ilia <75274656+iliazzz@users.noreply.github.com>
2026-04-16 22:58:17 +0300

    More minor fixes and new info

--- footer.js
+++ footer.js
@@ -8,7 +8,7 @@ document.write('\
                 <h3>September 17-20 <br> Chisinau, Moldova</h3>\
             </td>\
             <td id="fcentre">\
-                <h1>Sponsors and Partners TBD</h1>\
+                <h1>In cooperation with JEF Moldova</h1>\
             </td>\
             <td id="fright">\
                 <h2>Contact us:<br> For general questions: <br> <a href="mailto:secretariat@blackseamodelun.org">secretariat@blackseamodelun.org</a> <br><br><a href="https://www.instagram.com/blacksea.mun/" style="font-size: 1.5vw;"><i class="fa-brands fa-instagram" style="color: #0d0d0d;"></i></a> <a href="https://mymun.com/conferences/blackseamun-2026"><img src="mymun_128.png" class="mmlogo"></a> </h2>\
@@ -29,7 +29,7 @@ document.write('\
         </tr>\
         <tr>\
             <td id="fcentre">\
-                <h1>Sponsors and Partners TBD</h1>\
+                <h1>In cooperation with JEF Moldova</h1>\
             </td>\
         </tr>\
         <tr>    \
